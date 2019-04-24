6bb0a41b4ad0ce4dbb2c44360cd540b4c4c342d2
Kingsley John <nwankwo.kingsleyjohn@gmail.com>
2019-04-24 16:55:49 +0100

    create message broadcast reciever

--- app/src/main/java/kingsleyjohn/com/ven10/MessageReceiveClass.java
+++ app/src/main/java/kingsleyjohn/com/ven10/MessageReceiveClass.java
@@ -1,5 +1,13 @@
 package kingsleyjohn.com.ven10;
 
-public class MessageReceiveClass {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 
+public class MessageReceiveClass extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
 }
