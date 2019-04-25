b0a0b28530d115eadfeee17b01bc3b9ad7108cae
Kingsley John <nwankwo.kingsleyjohn@gmail.com>
2019-04-25 09:38:54 +0100

    add request permission for reading message

--- app/src/main/java/kingsleyjohn/com/ven10/Home.java
+++ app/src/main/java/kingsleyjohn/com/ven10/Home.java
@@ -1,13 +1,56 @@
 package kingsleyjohn.com.ven10;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements MessageListenerInterface {
+
+    static int PERMISSIONS_REQUEST_READ_SMS = 12736;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        requestReadMessagePermission();
+    }
+
+    @Override
+    public void messageReceived(String message) {
+        Toast.makeText(this, "New Message Received: " + message, Toast.LENGTH_SHORT).show();
+        Log.e("hello", message);
+    }
+
+    private void requestReadMessagePermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(
+                Manifest.permission.READ_SMS
+        ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(new String[]{Manifest.permission.READ_SMS}, PERMISSIONS_REQUEST_READ_SMS);
+        } else {
+            registerListener();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSIONS_REQUEST_READ_SMS && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            registerListener();
+        }
+    }
+
+    private void registerListener(){
+        //Register message listener
+        MessageReceiveClass.bindListener(this);
     }
 }
