fcf49bff1dee9ec300d75a487952379a4e2fe860
Kingsley John <nwankwo.kingsleyjohn@gmail.com>
2019-04-26 12:01:24 +0100

    change message processing to use regex

--- app/src/main/java/kingsleyjohn/com/ven10/CodedMessage.java
+++ app/src/main/java/kingsleyjohn/com/ven10/CodedMessage.java
@@ -12,6 +12,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CodedMessage extends AppCompatActivity {
 
@@ -31,44 +33,62 @@ public class CodedMessage extends AppCompatActivity {
         time = findViewById(R.id.time);
         codeMsg = findViewById(R.id.codedMessage);
 
+        String message = getIntent().getStringExtra("message");
+        Log.e("message", "hf  "+message);
+
          String length;
          String width;
          String firstColor;
          String secondColor;
+         String timeString;
+         String dateString;
+         String codedMessage;
 
-        String message = getIntent().getStringExtra("message");
-        Log.e("message", "hf  "+message);
+        final String secondLineRegex = "(?<=\\n)(.*)(?=\\n)";
+        final String timeRegex = "([0-9]|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])\\s*([AaPp][Mm])";
+        final String dateRegex = "([0-9]{2}[/]{1}[0-9]{2}[/]{1}[0-9]{4})";
+        final String lengthRegex = "(?<=w)(.*)(?=l)";
+        final String widthRegex = "(?<=SZ )(.*)(?=w)";
+        final String firstColorRegex = "(?<=/)(.*)(?=-)";
+        final String secondColorRegex = "(?<=-)(.*)(?=)";
+
+
+        final Pattern secondLinePattern = Pattern.compile(secondLineRegex, Pattern.MULTILINE);
+        final Pattern timePattern = Pattern.compile(timeRegex, Pattern.MULTILINE);
+        final Pattern datePattern = Pattern.compile(dateRegex, Pattern.MULTILINE);
+        final Pattern lengthPattern = Pattern.compile(lengthRegex, Pattern.MULTILINE);
+        final Pattern widthPattern = Pattern.compile(widthRegex, Pattern.MULTILINE);
+        final Pattern firstColorPattern = Pattern.compile(firstColorRegex, Pattern.MULTILINE);
+        final Pattern secondColorPattern = Pattern.compile(secondColorRegex, Pattern.MULTILINE);
+
+        firstColor = decodeMessage(firstColorPattern, message);
+        secondColor = decodeMessage(secondColorPattern, message);
+        width = decodeMessage(widthPattern, message);
+        length = decodeMessage(lengthPattern, message);
+        timeString = decodeMessage(timePattern, message);
+        dateString = decodeMessage(datePattern, message);
+        codedMessage = decodeMessage(secondLinePattern, message);
+
+
 
         //11/18/2016:7:37 AM
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy:H:mm a", Locale.UK);
-
-        String firstLine = message.split("\\n")[0];
-        String secondLine = message.split("\\n")[1];
-        String thirdLine = message.split("\\n")[2];
-
-
-        firstColor = thirdLine.split("/")[1].split("-")[0];
-        secondColor = thirdLine.split("/")[1].split("-")[1];
-
-        width = thirdLine.split("/")[0].split("Z")[1].split("w")[0];
-        String lengthSection = thirdLine.split("/")[0].split("Z")[1].split("w")[1];
-        length = lengthSection.substring(0, lengthSection.length()-2);
-
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy", Locale.UK);
 
 
         try{
-            Date msgDate = dateFormat.parse(firstLine.substring(3));
+            Date msgDate = dateFormat.parse(dateString);
             date.setText(getDate(msgDate));
-            time.setText(getTime(msgDate));
         }catch (Exception e){
             e.printStackTrace();
         }
 
         String imageUrl = "https://dummyimage.com/"+width.trim()+"x"+length.trim()+"/"+firstColor.trim()+"/"+secondColor.trim();
 
-        codeMsg.setText(secondLine);
+        codeMsg.setText(codedMessage);
         Picasso.with(this).load(imageUrl).into(imageView);
+        time.setText(timeString);
 
+        Log.e("image", imageUrl);
 
     }
 
@@ -78,10 +98,6 @@ public class CodedMessage extends AppCompatActivity {
         return "Date: "+c.get(Calendar.DATE)+getDayNumberSuffix(c.get(Calendar.DATE))+" "+c.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.UK)+ " "+c.get(Calendar.YEAR);
     }
 
-    private String getTime(Date date){
-        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm a", Locale.UK);
-        return "Time: "+timeFormat.format(date).toUpperCase();
-    }
 
     private String getDayNumberSuffix(int day) {
         if (day >= 11 && day <= 13) {
@@ -96,6 +112,15 @@ public class CodedMessage extends AppCompatActivity {
                 return "rd";
             default:
                 return "th";
+        }
+    }
+
+    private String decodeMessage(Pattern pattern, String message){
+        Matcher matcher = pattern.matcher(message);
+        if(matcher.find()){
+            return matcher.group(0);
+        }else{
+            return "Not found";
         }
     }
 }
